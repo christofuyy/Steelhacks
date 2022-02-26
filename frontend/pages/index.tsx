37783ec74143aa458edc666a61ac8a1360b18012
Christopher <cyoun@chapman.edu>
2022-02-26 15:21:44 -0800

    refactor: Add sign in and sign out links to home page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,25 @@
 import type { NextPage } from "next";
+import Button from "components/elements/Button";
+import { useAuth } from "contexts/AuthContext";
+import LoadingPage from "components/templates/LoadingPage";
+import Typography from "components/elements/Typography";
 
 const Home: NextPage = () => {
-  return <div>My App</div>;
+  const { loading, user } = useAuth();
+
+  if (loading) return <LoadingPage />;
+  return (
+    <div>
+      {user === null ? (
+        <Typography>Not authenticated</Typography>
+      ) : (
+        <Typography>Currently signed in as {user.email}</Typography>
+      )}
+      <Button href={user !== null ? "/auth/signout" : "/auth/signin"}>
+        {user !== null ? "Sign Out" : "Sign In"}
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
