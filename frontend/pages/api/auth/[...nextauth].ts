25eb4d22f9d703e87427d3ba5fbedf246a4c48d8
wonderhead <mitch.radman101@gmail.com>
2022-02-25 23:08:55 -0500

    added callbacks to nextauth

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -19,5 +19,17 @@ export default NextAuth({
   },
   jwt: {
     secret: process.env.SECRET,
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken
+      return session
+    }
   }
 })
