747ffd77ad095e038e59d2a5fb05a21b97c009f7
wonderhead <mitch.radman101@gmail.com>
2022-02-25 23:01:55 -0500

    Integrated google and github login to index

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,28 @@
 import type { NextPage } from 'next'
+import { signIn, signOut, useSession} from 'next-auth/react'
 
 const Home: NextPage = () => {
+  const {data: session, status} = useSession();
+  const loading = status === "loading"
+
+  if (loading){
+    return null
+  }
+
+  if (!loading && !session){
+    return (
+      <>
+        <div>BRO LOG IN</div>
+        <button onClick={() => signIn()}>Sign In</button>
+      </>
+
+    )
+  }
   return (
-    <div>Hello World</div>
+    <>
+      <div>Hello World</div>
+      <button onClick={() => signOut()}>Sign Out</button>
+    </>
   )
 }
 
