39d248bc14aa39ff0118abcd4c27b1fad6291db2
Christopher <cyoun@chapman.edu>
2022-02-26 15:49:30 -0800

    feat: Add authentication check to dashboard page

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from "next";
-
+import { useAuth } from "contexts/AuthContext";
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
 import Header from "components/modules/Dashboard/Header";
 import PageHeader from "components/modules/Dashboard/Header/PageHeader";
 import ImageForm from "components/modules/Dashboard/ImageForm";
+import LoadingPage from "components/templates/LoadingPage";
+import Redirect from "components/elements/Redirect";
 
 const Dashboard: NextPage = () => {
+  const { loading, user } = useAuth();
+
+  if (loading) return <LoadingPage />;
+  if (!user) return <Redirect to="/auth/signin" />;
   return (
     <>
       <Stack bgcolor="#EFF1F4" minHeight="100vh">
